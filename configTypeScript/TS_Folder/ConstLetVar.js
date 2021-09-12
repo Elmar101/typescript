c2149ebee5e4ebdee9de87f0f9afde6d7b479b14
Elmar Amanov <Elmar.amanov10@gmail.com>
2021-09-12 20:42:51 +0400

    let var const

--- configTypeScript/TS_Folder/ConstLetVar.js
+++ configTypeScript/TS_Folder/ConstLetVar.js
@@ -1,34 +1,26 @@
-const userName:string = "Elmar";
-
+var userName = "Elmar";
 /*/
                             {Cannot assign to 'userName' because it is a constant.
      userName = "Ruslan"; =>
                             {Sabit olduğu üçün 'userName' -ə yeni qiymet atana bilmez!
 /*/
-
-function text1():string {
+function text1() {
     var str = "Hello Ts";
     return str;
 }
-
 //console.log(str) => Cannot find name 'str'. - 'Str' adı tapılmadı. yeni str adinda deyisen yox
-
 var text; // yaza bilersiz
 function text2() {
-    text = "Hello Ts"; 
+    text = "Hello Ts";
 }
-
-console.log(text);//undefined
-
-let textStr; // yaza bilersiz
+console.log(text); //undefined
+var textStr; // yaza bilersiz
 function text3() {
     textStr = "Hello Ts";
 }
-
 console.log(textStr);
-
-let age: number = 40;
-if(age >= 18){
-    var txt = "yashiniz chatir qaqa !"
+var age = 40;
+if (age >= 18) {
+    var txt = "yashiniz chatir qaqa !";
 }
-console.log(txt);//"yashiniz chatir qaqa !"
+console.log(txt); //"yashiniz chatir qaqa !"
