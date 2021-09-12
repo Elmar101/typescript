fb06f4977a077ac8ca950506a9d471a246818559
Elmar Amanov <Elmar.amanov10@gmail.com>
2021-09-12 21:11:29 +0400

    some changes

---
+++ JS_NextGeneration_TS/ArrowFunction.js
@@ -0,0 +1,6 @@
+var arrowFn = function (n1, n2) { return console.log(n1 + n2); };
+var arrFn = function (output) { return console.log(output); };
+//let arrFn = output:string => console.log(output); - bele yaza bilmirik Cannot find name 'output'. sehfi olur
+var arFn = function (output) { return console.log(output); };
+//arrFnunction:(n1: string, n2: string) => void arrowFunction Type
+var arrFnunction = function (n1, n2) { return console.log(n1 + n2); };
